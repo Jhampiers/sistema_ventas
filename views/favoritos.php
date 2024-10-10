aa16cd6ead04d39967c89989bcf2701d0f9e3387
Yampiers Quispe <169065995+Jhampiers@users.noreply.github.com>
2024-10-10 11:49:05 -0500

    init

--- views/favoritos.php
+++ views/favoritos.php
@@ -3,7 +3,7 @@
               
               <ul class="favorites-list">
                   <li class="product-item">
-                      <img src="/api/placeholder/100/100" alt="Tablet Samsung Galaxy Tab S9+" class="product-image">
+                      <img src="https://promart.vteximg.com.br/arquivos/ids/7933417-1000-1000/imageUrl_1.jpg?v=638491439095600000" alt="Tablet Samsung Galaxy Tab S9+" class="product-image">
                       <div class="product-details">
                           <div class="product-title">Tablet Samsung Galaxy Tab S9+ FE 12.4" 8GB RAM 128GB</div>
                           <div class="product-model">SM-X610NZADPEO Gray</div>
@@ -16,7 +16,7 @@
                       </div>
                   </li>
                   <li class="product-item">
-                      <img src="/api/placeholder/100/100" alt="Tablet Samsung Galaxy Tab S9+" class="product-image">
+                      <img src="https://promart.vteximg.com.br/arquivos/ids/7933417-1000-1000/imageUrl_1.jpg?v=638491439095600000" alt="Tablet Samsung Galaxy Tab S9+" class="product-image">
                       <div class="product-details">
                           <div class="product-title">Tablet Samsung Galaxy Tab S9+ FE 12.4" 8GB RAM 128GB</div>
                           <div class="product-model">SM-X610NZADPEO Gray</div>
@@ -29,7 +29,7 @@
                       </div>
                   </li>
                   <li class="product-item">
-                      <img src="/api/placeholder/100/100" alt="Tablet Samsung Galaxy Tab S9+" class="product-image">
+                      <img src="https://promart.vteximg.com.br/arquivos/ids/7933417-1000-1000/imageUrl_1.jpg?v=638491439095600000" alt="Tablet Samsung Galaxy Tab S9+" class="product-image">
                       <div class="product-details">
                           <div class="product-title">Tablet Samsung Galaxy Tab S9+ FE 12.4" 8GB RAM 128GB</div>
                           <div class="product-model">SM-X610NZADPEO Gray</div>
@@ -42,7 +42,7 @@
                       </div>
                   </li>
                   <li class="product-item">
-                      <img src="/api/placeholder/100/100" alt="Tablet Samsung Galaxy Tab S9+" class="product-image">
+                      <img src="https://promart.vteximg.com.br/arquivos/ids/7933417-1000-1000/imageUrl_1.jpg?v=638491439095600000" alt="Tablet Samsung Galaxy Tab S9+" class="product-image">
                       <div class="product-details">
                           <div class="product-title">Tablet Samsung Galaxy Tab S9+ FE 12.4" 8GB RAM 128GB</div>
                           <div class="product-model">SM-X610NZADPEO Gray</div>
