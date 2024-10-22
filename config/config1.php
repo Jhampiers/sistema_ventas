de7ea95e9f4a518adedf763224c60591df1829d2
Yampiers Quispe <169065995+Jhampiers@users.noreply.github.com>
2024-10-22 10:17:09 -0500

    init

--- config/config1.php
+++ config/config1.php
@@ -3,7 +3,7 @@ const  DB_HOST = 'localhost';
 const  DB_NAME = '';
 const  DB_USER = '';
 const  DB_PASSWORD = '';
-const  BD_CHARSET = 'utf-8';
+const  DB_CHARSET = 'utf-8';
 //ingresar link de proyecto
 const  BASE_URL = '';
 
