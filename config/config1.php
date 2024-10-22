8ba4952e856aeab332006b33a00e9307ef8531ce
Yampiers Quispe <169065995+Jhampiers@users.noreply.github.com>
2024-10-22 10:19:27 -0500

    init

--- config/config1.php
+++ config/config1.php
@@ -1,9 +1,9 @@
 <?php
-const  DB_HOST = 'localhost';
-const  DB_NAME = '';
-const  DB_USER = '';
-const  DB_PASSWORD = '';
-const  DB_CHARSET = 'utf-8';
+const  BD_HOST = 'localhost';
+const  BD_NAME = '';
+const  BD_USER = '';
+const  BD_PASSWORD = '';
+const  BD_CHARSET = 'utf-8';
 //ingresar link de proyecto
 const  BASE_URL = '';
 
--- librerias/conexion.php
+++ librerias/conexion.php
@@ -2,13 +2,13 @@
 require_once "./config/config.php";
 class Conexion{
     public static function connect(){
-        $mysql = new mysqli(DB_HOST, DB_NAME, DB_USER, DB_PASSWORD);
-        $mysql->set_charset(DB_CHARSET);
+        $mysql = new mysqli(BD_HOST, BD_USER, BD_PASSWORD, BD_NAME);
+        $mysql->set_charset(BD_CHARSET);
         if (mysqli_connect_errno()){
             echo "Error de conexion: ".
             mysqli_connect_errno();
         }
-        return $msql;
+        return $mysql;
     }
 
 }
