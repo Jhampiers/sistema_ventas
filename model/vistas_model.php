9071b80af83992403b1df87b042904cdb4b0cd44
Yampiers Quispe <169065995+Jhampiers@users.noreply.github.com>
2024-11-26 14:03:05 -0500

    init

--- model/vistas_model.php
+++ model/vistas_model.php
@@ -3,9 +3,9 @@ session_start();
 class vistaModelo{
     protected static function obtener_vista($vista){
        $palabras_permitidas =['usuario','producto','producto2','producto3','contacto','nosotros','index','favoritos','carrito','detalle','login','nuevo-producto','nuevo-categoria','nuevo-persona','nuevo-compra','productos','categorias','personas','compras'];
-        if(!isset($_SESSION['sesion_ventas_id'])){
-            return "login";
-        }
+        // if(!isset($_SESSION['sesion_ventas_id'])){
+        //     return "login";
+        // }
 
        if (in_array($vista,$palabras_permitidas)){
             if(is_file("./views/".$vista.".php")){
